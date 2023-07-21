c8aed35cd892adaff73e51e80fb4d288c70040af
Wtell64 <75840262+Wtell64@users.noreply.github.com>
2023-07-21 13:45:03 +0300

    Program.cs dosyasinda patternde yapilan hata silindi.

--- AspNetMvcBlog/App.Web.Mvc/Program.cs
+++ AspNetMvcBlog/App.Web.Mvc/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}/{query?}/{page?}{redirectUrl?}");
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
